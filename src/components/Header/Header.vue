486c49e91b7f1de673abe502f466893ae7502993
kunal-lohana <kunal.lohana84@gmail.com>
2020-09-11 21:15:27 +0530

    Implement search functionality and fixed bug

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -15,24 +15,49 @@
     </div>
 
     <v-spacer></v-spacer>
-
-    <v-text-field
-      name="search"
-      label="Search..."
-      v-model="searchInput"
-      clearable
-      prepend-inner-icon="mdi-magnify"
-    ></v-text-field>
+    <v-container>
+      <v-row class="justify-end">
+        <v-col cols="8">
+          <v-text-field
+            max-width="50"
+            name="search"
+            label="Search..."
+            v-model="searchInput"
+            clearable
+            prepend-inner-icon="mdi-magnify"
+            @keyup.enter="getSearchQuery()"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
 <script>
+import { fetchData } from "@/api/fetchData.js";
+import { SEARCH_SHOW } from "@/api/apiName.js";
+import { EventBus } from "@/components/EventBus.js";
 export default {
   name: "Header",
   data() {
     return {
-      searchInput: ""
+      searchInput: "",
+      searchedData: []
     };
+  },
+  methods: {
+    async getSearchQuery() {
+      if (this.searchInput) {
+        const response = await fetchData({
+          apiName: SEARCH_SHOW,
+          params: { query: this.searchInput }
+        });
+        if (response.data.length) {
+          this.searchedData = response.data;
+          EventBus.$emit("search-query-event", this.searchedData);
+        }
+      }
+    }
   }
 };
 </script>
